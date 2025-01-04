8445720bfcae73a376736760e64d3f7d6a1f0133
thilinacz <koggalage@gmail.com>
2025-01-04 14:58:43 +0530

    #Admin Sidebar

--- app/views/eshop/admin/sidebar.php
+++ app/views/eshop/admin/sidebar.php
@@ -13,14 +13,68 @@
               	  	
                   <li class="sub-menu">
                       <a href="javascript:;" >
-                          <i class="fa fa-desktop"></i>
-                          <span>UI Elements</span>
+                      <i class="fa fa-dashboard"></i>
+                      <span>Dashboard</span>
+                      </a>
+                  </li>
+
+                  <li class="sub-menu">
+                      <a href="<?= ROOT ?>admin/products" >
+                      <i class="fa fa-barcode"></i>
+                      <span>Products</span>
                       </a>
                       <ul class="sub">
-                          <li><a  href="general.html">General</a></li>
-                          <li><a  href="buttons.html">Buttons</a></li>
-                          <li><a  href="panels.html">Panels</a></li>
+                          <li><a  href="<?= ROOT ?>admin/products/add">Add New Product</a></li>
+                          <li><a  href="<?= ROOT ?>admin/products/edit">Edit Product</a></li>
+                          <li><a  href="<?= ROOT ?>admin/products/delete">Delete Product</a></li>
                       </ul>
+                  </li>
+
+                  <li class="sub-menu">
+                      <a href="<?= ROOT ?>admin/categories" >
+                      <i class="fa fa-list-alt"></i>
+                      <span>Categories</span>
+                      </a>
+                      <ul class="sub">
+                          <li><a  href="<?= ROOT ?>admin/categories/add">Add New Category</a></li>
+                          <li><a  href="<?= ROOT ?>admin/categories/edit">Edit Category</a></li>
+                          <li><a  href="<?= ROOT ?>admin/categories/delete">Delete Category</a></li>
+                      </ul>
+                  </li>
+
+                  <li class="sub-menu">
+                      <a href="<?= ROOT ?>admin/orders" >
+                      <i class="fa fa-reorder"></i>
+                      <span>Order</span>
+                      </a>
+                  </li>
+
+                  <li class="sub-menu">
+                      <a href="<?= ROOT ?>admin/settings" >
+                      <i class="fa fa-cogs"></i>
+                      <span>Settings</span>
+                      </a>
+                      <ul class="sub">
+                          <li><a  href="<?= ROOT ?>admin/settings/slider_images">Slider Images</a></li>
+                      </ul>
+                  </li>
+
+                  <li class="sub-menu">
+                      <a href="<?= ROOT ?>admin/users" >
+                      <i class="fa fa-cogs"></i>
+                      <span>Users</span>
+                      </a>
+                      <ul class="sub">
+                          <li><a  href="<?= ROOT ?>admin/users/customers">Customers</a></li>
+                          <li><a  href="<?= ROOT ?>admin/users/admins">Admins</a></li>
+                      </ul>
+                  </li>
+
+                  <li class="sub-menu">
+                      <a href="<?= ROOT ?>admin/backup" >
+                      <i class="fa fa-hdd-o"></i>
+                      <span>Website Backup</span>
+                      </a>
                   </li>
 
               </ul>
